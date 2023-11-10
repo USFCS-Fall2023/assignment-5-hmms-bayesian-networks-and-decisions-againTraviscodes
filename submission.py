fafaf61c2e18c3954678d190382e2e4ff70d51b8
againTraviscodes <t.uni.versity17@gmail.com>
2023-11-09 17:44:42 -0800

    p02 #04 viterbi added to submission.py

--- submission.py
+++ submission.py
@@ -5,25 +5,33 @@ from alarm import belief_networks_alarm as bn_a
 from carnet import belief_networks_carnet as bn_c
 
 
-# def main():
-#     # TODO: make cli callable
-#     hmm = HMM()
-#     hmm.load('two_english')
-#     print(hmm.transitions)
-#     print(hmm.emissions)
-
-
 if __name__ == '__main__':
+    # Part 2: Hidden Markov Models
+    # Section 1: Load
+    print("\nLoad")
     hmm = HMM()
-    hmm.load('partofspeech.browntags.trained')  # 'partofspeech.browntags.trained' 'two_english'
+    hmm.load('partofspeech.browntags.trained')
+    # hmm.load('two_english')
 
-    observation = hmm.generate(20)  # n = 20, changeable
+    # Section 2: Generate
+    print("\nGenerate")
+    observation = hmm.generate(20)
     print(observation)
 
-    obsrv = hmm.load_observation('ambiguous_sents.obs')  # 'ambiguous_sents.obs' 'english_words.obs'
-    for o in obsrv:
-        print(hmm.forward(o))
-    # print(hmm.forward(observation.outputseq))
+    # Section 3: Forward
+    print("\nForward")
+    o = hmm.load_observation('ambiguous_sents.obs')  # TODO make entire file = 1 observation
+    # o = hmm.load_observation('english_words.obs')
+    print(hmm.forward(o))
 
-    # bn_a()
-    # bn_c()
+    # Section 4: Viterbi
+    print("\nViterbi")
+    print(hmm.viterbi(o))
+
+    # Part 3: Belief Networks
+    # Sections 1 & 2:
+    print("\nBelief Networks: Alarm")
+    bn_a()
+    # Section 3:
+    print("\nBelief Networks: Carnet")
+    bn_c()
